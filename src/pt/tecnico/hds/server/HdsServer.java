52caf1436eba544b7a22a24c359bd4f87714db3a
lolstorm92 <ricardo.s.campos@tecnico.ulisboa.pt>
2019-03-11 22:27:05 +0000

    Initial commit

--- src/pt/tecnico/hds/server/HdsServer.java
+++ src/pt/tecnico/hds/server/HdsServer.java
@@ -1,5 +1,6 @@
 package pt.tecnico.hds.server;
 
+import java.io.*;
 import java.net.Socket;
 
 public class HdsServer implements Runnable {
@@ -13,7 +14,36 @@ public class HdsServer implements Runnable {
     }
 
     public void run() {
-
+        try {
+            BufferedInputStream is = new BufferedInputStream(connection.getInputStream());
+            InputStreamReader isr = new InputStreamReader(is);
+            int character;
+            StringBuffer process = new StringBuffer();
+            while((character = isr.read()) != 13) {
+                process.append((char)character);
+            }
+            System.out.println(process);
+            //need to wait 10 seconds to pretend that we're processing something
+            try {
+                Thread.sleep(10000);
+            }
+            catch (Exception e){}
+            TimeStamp = new java.util.Date().toString();
+            String returnCode = "MultipleSocketServer repsonded at "+ TimeStamp + (char) 13;
+            BufferedOutputStream os = new BufferedOutputStream(connection.getOutputStream());
+            OutputStreamWriter osw = new OutputStreamWriter(os, "US-ASCII");
+            osw.write(returnCode);
+            osw.flush();
+        }
+        catch (Exception e) {
+            System.out.println(e);
+        }
+        finally {
+            try {
+                connection.close();
+            }
+            catch (IOException e){}
+        }
     }
 
 }
