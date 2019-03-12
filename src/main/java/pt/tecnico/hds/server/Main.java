ee1cbfa962e8dc06b732783b51c2337ea78f5aaa
lolstorm92 <ricardo.s.campos@tecnico.ulisboa.pt>
2019-03-12 02:40:14 +0000

    Added command line commands

--- src/main/java/pt/tecnico/hds/server/Main.java
+++ src/main/java/pt/tecnico/hds/server/Main.java
@@ -1,5 +1,7 @@
 package pt.tecnico.hds.server;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -8,13 +10,17 @@ public class Main {
     public static void main(String[] args) {
         int port = 19999;
         int count = 0;
-        try{
+        try {
             ServerSocket socket1 = new ServerSocket(port);
             socket1.setReuseAddress(true);
             System.out.println("HDS Notary Server Initialized");
             while (true) {
                 Socket connection = socket1.accept();
-                Runnable runnable = new HdsServer(connection, ++count);
+
+                // obtaining input and out streams
+                DataInputStream dis = new DataInputStream(connection.getInputStream());
+                DataOutputStream dos = new DataOutputStream(connection.getOutputStream());
+                Runnable runnable = new HdsServer(connection, ++count, dis, dos);
                 Thread thread = new Thread(runnable);
                 thread.start();
             }
