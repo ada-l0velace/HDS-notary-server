d5efdd2b4d22d7711bea47da1b2f91f62a6b5833
nucleardannyd <daniel.leitao@ist.utl.pt>
2019-03-12 03:16:06 +0000

    creates database now

--- src/main/java/pt/tecnico/hds/server/Main.java
+++ src/main/java/pt/tecnico/hds/server/Main.java
@@ -2,13 +2,18 @@ package pt.tecnico.hds.server;
 
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.sql.*;
+
+import static java.lang.System.exit;
 
 public class Main {
+
 
     public static void main(String[] args) {
         int port = 19999;
         int count = 0;
         try{
+            createDatabase();
             ServerSocket socket1 = new ServerSocket(port);
             socket1.setReuseAddress(true);
             System.out.println("HDS Notary Server Initialized");
@@ -23,4 +28,28 @@ public class Main {
             System.out.println(e.getMessage());
         }
     }
+
+    public static Connection createDatabase(){
+
+        String url = "jdbc:sqlite:db/hds.db";
+
+        // SQL statement for creating a new table
+        String sql = "CREATE TABLE IF NOT EXISTS notary (\n"
+                + "	goodsId text PRIMARY KEY,\n"
+                + "	userId text NOT NULL,\n"
+                + "	onSale boolean NOT NULL\n"
+                + ");";
+
+        try{
+            Connection conn = DriverManager.getConnection(url);
+            Statement stmt = conn.createStatement();
+            // create a new table
+            stmt.execute(sql);
+            return conn;
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return null;
+    }
+
 }
