32d3c824cf9b74ada5eefa8b79cc9b2d775c025e
nucleardannyd <daniel.leitao@ist.utl.pt>
2019-03-22 01:56:43 +0000

    Refactored code

--- src/main/java/pt/tecnico/hds/server/HdsServer.java
+++ src/main/java/pt/tecnico/hds/server/HdsServer.java
@@ -4,30 +4,20 @@ import java.io.*;
 import java.net.Socket;
 import java.net.SocketException;
 import java.sql.*;
-import java.text.DateFormat;
 
 import org.json.JSONObject;
 
 public class HdsServer implements Runnable {
 
-    private static final String url = "jdbc:sqlite:db/hds.db";
 
     private Socket connection;
     private String TimeStamp;
     private int ID;
     private DataInputStream dis;
+    private Notary nt = new Notary();
     private DataOutputStream dos;
 
-    private Connection connect() {
-        // SQLite connection string
-        Connection conn = null;
-        try {
-            conn = DriverManager.getConnection(url);
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        }
-        return conn;
-    }
+
 
     public HdsServer(Socket s, int i, DataInputStream dis, DataOutputStream dos) {
         this.connection = s;
@@ -62,14 +52,14 @@ public class HdsServer implements Runnable {
                 switch (received) {
 
                     case "transferGood":
-                        message = transferGood(jsonObj, hash);
+                        message = nt.transferGood(jsonObj, hash);
                         toreturn = buildReply(message).toString();
                         dos.writeUTF(toreturn);
                         System.out.println(toreturn);
                         break;
 
                     case "intentionToSell":
-                        message = intentionToSell(jsonObj, hash);
+                        message = nt.intentionToSell(jsonObj, hash);
                         //System.out.println(toreturn);
                         toreturn = buildReply(message).toString();
                         dos.writeUTF(toreturn);
@@ -77,7 +67,7 @@ public class HdsServer implements Runnable {
                         break;
 
                     case "getStateOfGood":
-                        message = getStateOfGood(jsonObj.getString("Good"));
+                        message = nt.getStateOfGood(jsonObj.getString("Good"));
                         toreturn = buildReply(message).toString();
                         dos.writeUTF(toreturn);
                         System.out.println(toreturn);
@@ -120,52 +110,6 @@ public class HdsServer implements Runnable {
         }
     }
 
-    public boolean isReal(String type, String table, String id, Connection conn) {
-        String sql = "SELECT " + type + " FROM " + table + " WHERE " + type + " = ?";
-        try {
-            PreparedStatement pstmt = conn.prepareStatement(sql);
-            pstmt.setString(1, id);
-            ResultSet rs = pstmt.executeQuery();
-            if (rs.next())
-                return true;
-            System.out.println(type + " " + id + " nonexistent");
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        }
-        return false;
-    }
-
-    public boolean isOwner(String owner, String good, Connection conn) {
-        String sql = "SELECT userId FROM notary WHERE goodsId = ?";
-        try {
-            PreparedStatement pstmt = conn.prepareStatement(sql);
-            pstmt.setString(1, good);
-            ResultSet rs = pstmt.executeQuery();
-            if (rs.next() && rs.getString("userId").equals(owner))
-                return true;
-            System.out.println(owner + " IS NOT OWNER OF " + good);
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        }
-        return false;
-    }
-
-    public boolean isOnSale(String good, Connection conn) {
-        String sql = "SELECT onSale FROM notary WHERE goodsId = ?";
-
-        try {
-            PreparedStatement pstmt = conn.prepareStatement(sql);
-
-            pstmt.setString(1, good);
-            ResultSet rs = pstmt.executeQuery();
-            if (rs.next()) {
-                return rs.getBoolean("onSale");
-            }
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        }
-        return false;
-    }
 
     public JSONObject buildReply(JSONObject j) {
         JSONObject reply = new JSONObject();
@@ -174,152 +118,4 @@ public class HdsServer implements Runnable {
         return reply;
     }
 
-    public JSONObject getStateOfGood(String good) {
-        String sql = "SELECT userId, onSale FROM notary WHERE goodsId = ?";
-        JSONObject reply = new JSONObject();
-        Boolean result = false;
-
-        try (Connection conn = this.connect();
-             PreparedStatement pstmt = conn.prepareStatement(sql)) {
-
-            pstmt.setString(1, good);
-            ResultSet rs = pstmt.executeQuery();
-            if (rs.next()) {
-                result = rs.getBoolean("onSale");
-                String user = rs.getString("userId");
-                reply.put("OnSale", result.toString());
-                reply.put("Owner", user);
-                reply.put("Good", good);
-
-            }
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        }
-        return reply;
-    }
-
-    public JSONObject transferGood(JSONObject message, String hash) {
-        String seller = message.getString("Seller");
-        JSONObject reply = new JSONObject();
-        if (Utils.verifySignWithPubKey(message.toString(), hash, "assymetricKeys/" + seller + ".pub")) {
-            String buyer = message.getString("Buyer");
-            String good = message.getString("Good");
-
-            String sql = "UPDATE notary SET onSale = FALSE , userId = ? WHERE goodsId = ?";
-            System.out.println(buyer + " " + seller);
-
-            try (Connection conn = this.connect();
-                 PreparedStatement pstmt = conn.prepareStatement(sql)) {
-                if (isReal("userId", "users", buyer, conn) &&
-                        isReal("userId", "users", seller, conn) &&
-                        isReal("goodsId", "goods", good, conn) &&
-                        isOnSale(good, conn) &&
-                        !buyer.equals(seller)) {
-                    pstmt.setString(1, buyer);
-                    pstmt.setString(2, good);
-                    pstmt.executeUpdate();
-                    reply.put("Action", "YES");
-                } else {
-                    reply.put("Action", "NO");
-                }
-            } catch (SQLException e) {
-                System.out.println(e.getMessage());
-                reply.put("Action", "NO");
-            }
-        } else {
-            reply.put("Action", "NO");
-        }
-        return reply;
-    }
-
-    public JSONObject intentionToSell(JSONObject message, String hash) {
-        String seller = message.getString("Seller");
-        JSONObject reply = new JSONObject();
-
-        if (Utils.verifySignWithPubKey(message.toString(), hash, "assymetricKeys/" + seller + ".pub")) {
-
-
-            String goodsId = message.getString("Good");
-
-            String sql = "UPDATE notary SET onSale = ? WHERE goodsId = ?";
-
-
-            try (Connection conn = this.connect();
-                 PreparedStatement pstmt = conn.prepareStatement(sql)) {
-                if (verifyReplay(hash, conn)) {
-                    System.out.println("Hash verified");
-                    query();
-                    addToRequests(hash, conn);
-                    if (isReal("goodsId", "goods", goodsId, conn) &&
-                            isOwner(seller, goodsId, conn)) {
-
-                        pstmt.setBoolean(1, true);
-                        pstmt.setString(2, goodsId);
-                        pstmt.executeUpdate();
-                        //query();
-                        reply.put("Action", "YES");
-                    } else
-                        reply.put("Action", "NO");
-                } else {
-                    reply.put("Action", "NO");
-                }
-
-            } catch (SQLException e) {
-                reply.put("Action", "NO");
-                System.out.println(e.getMessage());
-            }
-        } else {
-            reply.put("Action", "NO");
-        }
-        reply.put("Timestamp", new java.util.Date().toString());
-        return reply;
-    }
-
-    public void query() {
-        try {
-            Connection conn = this.connect();
-//            String sql = "SELECT goodsId, userId, onSale FROM notary";
-            String sql = "SELECT requestId FROM requests";
-
-            Statement stmt = conn.createStatement();
-            ResultSet rs = stmt.executeQuery(sql);
-            // Only expecting a single result
-            while (rs.next()) {
-//                System.out.println(rs.getString("goodsId") +  "\t" +
-//                        rs.getString("userId") + "\t" +
-//                        rs.getBoolean("onSale"));
-                System.out.println(rs.getString("requestId"));
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public static boolean verifyReplay(String hash, Connection conn) {
-        String sql = "SELECT requestId FROM requests WHERE requestId=?";
-        try {
-            PreparedStatement pstmt = conn.prepareStatement(sql);
-            pstmt.setString(1, hash);
-            ResultSet rs = pstmt.executeQuery();
-            if (rs.next()) {
-                return false;
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-            return false;
-        }
-        return true;
-    }
-
-    public static void addToRequests(String hash, Connection conn){
-        String sql = "INSERT INTO requests(requestId) Values(?)";
-
-        try {
-            PreparedStatement pstmt = conn.prepareStatement(sql);
-            pstmt.setString(1, hash);
-            pstmt.executeUpdate();
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        }
-    }
 }
